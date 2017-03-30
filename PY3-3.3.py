57c3ad2c3a6737da04542c6c325f35015089fce0
Vsevolod <vs.kamyshev@gmail.com>
2017-03-30 20:44:13 +0300

    Добавлена функция, использующая все предыдущие (для чтения, перевода и записи в файл. Доработана декомпозиция

--- PY3-3.3.py
+++ PY3-3.3.py
@@ -1,45 +1,37 @@
 import requests
 
+KEY = 'trnsl.1.1.20170330T165215Z.8f2572a45578ae5f.efcf9b1afa81dd18491f836c4e630b284c834770'
+URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
-key = 'trnsl.1.1.20170330T165215Z.8f2572a45578ae5f.efcf9b1afa81dd18491f836c4e630b284c834770'
-url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-
-def translate_it(text, from_lang, to_lang):
-    """
-    https://translate.yandex.net/api/v1.5/tr.json/translate ?
-    key=<API-ключ>
-     & text=<переводимый текст>
-     & lang=<направление перевода>
-     & [format=<формат текста>]
-     & [options=<опции перевода>]
-     & [callback=<имя callback-функции>]
-
-    :param to_lang:
-    :return:
-    """
-
-    params = {
-        'key': key,
-        'text': text,
-        'lang': '{}-{}'.format(from_lang, to_lang),
-    }
-
-    response = requests.get(url, params=params)
-    return ''.join(response.json()['text'])
-
-def file_to_str (file):
+# функция для чтения файла и записи его в переменную
+def file_to_str(file):
     text_to_translate = ''
     with open(file, 'r') as f:
         for line in f:
             text_to_translate += line
     return text_to_translate
 
-def str_to_file (translated_text, file):
+# функция для записи строки в файл
+def str_to_file(text, file):
     with open(file, 'w') as f:
-        f.write(translated_text)
-translated_text = (translate_it(file_to_str('./input files/ES.txt'), 'es', 'ru'))
-print(translated_text)
-print(type(translated_text))
+        f.write(text)
 
-str_to_file(translated_text, './output files/ES.txt')
-# print(translate_it('Что это такое?', 'de'))
+# функция перевода
+def translate_it(text, from_lang, to_lang='ru'):
+
+    params = {
+        'key': KEY,
+        'text': text,
+        'lang': '{}-{}'.format(from_lang, to_lang),
+    }
+
+    response = requests.get(URL, params=params)
+    return ''.join(response.json()['text'])
+
+# функция читает исходный файл, переводит и записыват в указанный файл
+def translate_to_file(input_file, output_file, from_lang, to_lang='ru'):
+    input_text = file_to_str(input_file)
+    translated_text = translate_it(input_text, from_lang, to_lang)
+    str_to_file(translated_text, output_file)
+
+translate_to_file('./input files/FR.txt', './output files/FR.txt', 'fr')
